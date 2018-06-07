21a1c2749f8369e8f3b2edfcc6089426c1983a8c
lahaymd <lahaymd@gmail.com>
2018-06-07 02:03:19 -0500

    deploy13

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -34,7 +34,7 @@ export const  updateRedStart = (redValues) => dispatch => {
     console.log('updating red values');
     // let data = { redState:redValues};
     
-    fetch('/green_screen', {
+    fetch('https://green-screen-api.herokuapp.com/green_screen', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
